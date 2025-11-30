c7c6c8bee964e39e1223cc53346491c0b7df67ca
KindoraHQ <154450659+KindoraHQ@users.noreply.github.com>
2025-11-30 16:15:31 +0100

    Rename KNRStagePresale to KINDORA-PRESALE

--- contracts/Presale.sol
+++ contracts/Presale.sol
@@ -694,7 +694,7 @@ interface IUniswapV2Router02 {
         returns (uint amountToken, uint amountETH, uint liquidity);
 }
 
-contract KNRStagePresale is Ownable, ReentrancyGuard {
+contract KINDORA-PRESALE is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     IERC20 public immutable saleToken;
